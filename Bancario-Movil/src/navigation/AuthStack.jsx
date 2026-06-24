fe8e2e790bc05227a2a38e1613890fa30faa9ad8
ZimriJahdai <zlopez-2023241@kinal.edu.gt>
2026-06-23 18:55:17 -0600

    C07 se completo pagina principal del proyecto

--- Bancario-Movil/src/navigation/AuthStack.jsx
+++ Bancario-Movil/src/navigation/AuthStack.jsx
@@ -4,12 +4,14 @@ import { LoginScreen } from '../features/auth/screens/LoginScreen';
 import { RegisterScreen } from '../features/auth/screens/RegisterScreen';
 import { VerifyEmailScreen } from '../features/auth/screens/VerifyEmailScreen';
 import { ForgotPasswordScreen } from '../features/auth/screens/ForgotPasswordScreen';
+import { WelcomeScreen } from '../features/auth/screens/WelcomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 export function AuthStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="VerifyEmail" component={VerifyEmailScreen} />
